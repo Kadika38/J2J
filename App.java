f49ec67f2221eb77fedc1b7193281b402f472875
David <dryan10101@gmail.com>
2023-05-15 16:16:42 -0700

    tests in App updated for new additions

--- App.java
+++ App.java
@@ -9,12 +9,11 @@ import com.github.kadika38.JSONBucket;
 public class App {
 
     public static void main(String[] args) {
-        /* JSONBucket testBucket = new JSONBucket(getJson());
-        System.out.println(testBucket.getOriginalJSON());
-        System.out.println(testBucket.getValue("validity_checks")); */
+        /* JSONBucket testBucket2 = new JSONBucket(getJson());
+        testBucket2.print(); */
 
-        JSONBucket testBucket2 = new JSONBucket(getJson());
-        testBucket2.print();
+        JSONBucket testBucket3 = JSONBucket.build(getJson());
+        testBucket3.print();
     }
 
     public static String getJson() {
